9119921a7cc7a34d1f49956c3f0c3e4a17fa38d2
Joe Miehl <joe.miehl@gmail.com>
2024-02-20 19:34:37 -0500

    Initial Commit

---
+++ HockeyPlayerStats/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HockeyPlayerStats
+//
+//  Created by Joe Miehl on 2/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HockeyPlayerStats/HockeyPlayerStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HockeyPlayerStatsApp.swift
+//  HockeyPlayerStats
+//
+//  Created by Joe Miehl on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct HockeyPlayerStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
